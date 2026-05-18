554a630e6ba4da859b4fb2efae2b726e6ce69460
Vuong <tranlevuongwir@gmail.com>
2026-05-18 16:44:08 +0700

    constants.py

---
+++ src/game/constants.py
@@ -0,0 +1,20 @@
+BOARD_SIZE = 15
+WIN_LENGTH = 5
+
+EMPTY = 0
+HUMAN = 1
+AI = -1
+
+DRAW = 2
+
+INF = 10**18
+
+MAX_DEPTH = 4
+
+# Directions
+DIRECTIONS = [
+    (0, 1),    # ngang
+    (1, 0),    # dọc
+    (1, 1),    # chéo xuống
+    (-1, 1)    # chéo lên
+]
